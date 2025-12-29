9744a3cd41f7a720a427b79c138c3ac7e3876150
Akil Khatri <khatriakil104@gmail.com>
2025-12-29 11:15:38 +0530

    Remove greeting logic from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import Image from "next/image";
-import { contact } from "./data";
-import Link from "next/link";
 import Introduction from "@/components/Introduction";
 import Skills from "@/components/Skills";
 
 export default function Home() {
-  const hour = new Date().getHours();
-
-  let greeting;
-
-  if (hour < 12) {
-    greeting = "Good Morning";
-  } else if (hour < 18) {
-    greeting = "Good Afternoon";
-  } else {
-    greeting = "Good Evening";
-  }
   return (
     <main className="flex flex-col bg-background/10">
       <Introduction />
